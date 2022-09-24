cf194cfb6620a14935816806b0ca4b6d04c39fc7
amitsethii <amitsethi150@gmail.com>
2022-09-24 14:15:58 +0530

    Delete mysqlDtabase directory

--- mysqlDtabase/src/main/java/com/mysqlDtabase/mysqlDtabase/dao/StudentsDao.java
+++
@@ -1,9 +0,0 @@
-package com.mysqlDtabase.mysqlDtabase.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mysqlDtabase.mysqlDtabase.entity.Students;
-
-public interface StudentsDao extends JpaRepository<Students, Integer> {
-
-}
